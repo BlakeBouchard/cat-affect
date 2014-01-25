08b7732449c73bc6dc9de03ea0c563d82ab1145c
Thomas Curnow <curnow@ualberta.ca>
2014-01-25 12:18:19 -0700

    Added in Main character flipping (left & right)

--- Grand-Effects/Assets/Scripts/PlayerControl.cs
+++ Grand-Effects/Assets/Scripts/PlayerControl.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 
     //animator variables
+    //refrence for movement and animating: http://www.youtube.com/watch?v=Xnyb2f6Qqzg
     bool facingRight = true;
 
     public float pushForce = 5.0f;
@@ -11,7 +12,7 @@ public class PlayerControl : MonoBehaviour {
     public float normalGravity;
     public float maxSpeed = 20.0f;
 
-    public bool isSwimming = true;
+    public bool isSwimming = false;
     public float swimForce = 2.0f;
     public float swimJumpForce = 2.0f;
     public float swimGravity = 0.3f;
@@ -35,9 +36,21 @@ public class PlayerControl : MonoBehaviour {
         isSwimming = false;
         rigidbody2D.gravityScale = normalGravity;
     }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+
+
+        float move = Input.GetAxis("Horizontal");
 
         if (isSwimming)
         {
@@ -77,5 +90,18 @@ public class PlayerControl : MonoBehaviour {
                 rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpForce);
             }
         }
+
+       
+        //direction flipper
+        if (move > 0 &&!facingRight)
+        {
+            
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+
 	}
 }
