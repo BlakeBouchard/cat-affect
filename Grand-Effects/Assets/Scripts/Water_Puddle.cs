84a238a3f4d5f2d49c19659fd2e166a09f81e0a6
afroholic78 <mickael.zerihoun@gmail.com>
2014-01-24 22:48:44 -0700

    Added test water puddle

---
+++ Grand-Effects/Assets/Scripts/Water_Puddle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Water_Puddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OntriggerEnter(Collider other) { 
+        //add the audio for water fill up 
+
+    }
+}
