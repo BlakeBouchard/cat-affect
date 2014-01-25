e71770337a9452b8975076718f9f054a506aa4b2
afroholic78 <mickael.zerihoun@gmail.com>
2014-01-25 11:31:37 -0700

    Added rising water
    Can be customized for different levels.

--- Grand-Effects/Assets/Scripts/Water_Puddle.cs
+++ Grand-Effects/Assets/Scripts/Water_Puddle.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Water_Puddle : MonoBehaviour {
 
+    public bool puddle_in_use = false;
+    public Transform Brick;
 	// Use this for initialization
 	void Start () {
 	
@@ -13,8 +15,16 @@ public class Water_Puddle : MonoBehaviour {
 	
 	}
 
-    void OntriggerEnter(Collider other) { 
-        //add the audio for water fill up 
+    void OnTriggerEnter2D(Collider2D other) { 
+        if(!puddle_in_use){
+            for (var y = 0; y < 5; y++)
+            {
+                for (var x = 0; x < 5; x++)
+                {
+                    Instantiate(Brick, new Vector3(x, y, 0), Quaternion.identity);
+                }
+            }
+        }
 
     }
 }
---
+++ Grand-Effects/Assets/Scripts/Water_flow.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Water_flow : MonoBehaviour {
+
+    private float y = 1;
+    private float x = 1;
+    private float water_rate = 0.5F;
+    private float water = 0.0F;
+    private bool right = true;
+    private bool left = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Debug.Log(Time.time);
+        if (Time.time > water)
+        {
+            
+            water = Time.time + water_rate;
+            if (right)
+            {
+                
+                //x += 1.0F;
+                transform.position += new Vector3(x, 0, 0);
+                Debug.Log(transform.position);
+                right = false;
+                
+            }
+
+            else 
+            {
+                transform.position -= new Vector3(x, 0, 0);
+                Debug.Log(transform.position);
+                right = true;
+                
+            }
+
+            transform.position += new Vector3(0, y, 0);
+        }
+
+            
+            
+            //y = y + 0.1;
+    
+
+	}
+}
