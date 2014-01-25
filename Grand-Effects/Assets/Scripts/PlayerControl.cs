e1c56d23b0811b9faa2afdfd0ad18ab3af2202dd
Thomas Curnow <curnow@ualberta.ca>
2014-01-25 09:45:16 -0700

    Added basic swimming controls
    (still needs a cap on max velocity)

--- Grand-Effects/Assets/Scripts/PlayerControl.cs
+++ Grand-Effects/Assets/Scripts/PlayerControl.cs
@@ -6,6 +6,11 @@ public class PlayerControl : MonoBehaviour {
     public float pushForce = 5.0f;
     public float jumpForce = 10.0f;
 
+    public bool isSwimming = true;
+    public float swimForce = 2.0f;
+    public float swimJumpForce = 2.0f;
+    public float swimGravity = 0.3f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -13,17 +18,40 @@ public class PlayerControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.LeftArrow))
+
+        if (isSwimming)
         {
-            rigidbody2D.AddForce(new Vector2(-pushForce, 0));
+
+            rigidbody2D.gravityScale = swimGravity;
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                rigidbody2D.AddForce(new Vector2(-swimForce, 0));
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                rigidbody2D.AddForce(new Vector2(swimForce, 0));
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, swimJumpForce);
+            }
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else
         {
-            rigidbody2D.AddForce(new Vector2(pushForce, 0));
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpForce);
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                rigidbody2D.AddForce(new Vector2(-pushForce, 0));
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                rigidbody2D.AddForce(new Vector2(pushForce, 0));
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpForce);
+            }
         }
 	}
 }
