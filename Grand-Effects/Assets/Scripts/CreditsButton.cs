6b4b88ad037f451491eb3fc129135ebe0a540eaf
Blake Bouchard <thatsblake@gmail.com>
2014-01-26 14:11:45 -0700

    Credits Button and Screen

---
+++ Grand-Effects/Assets/Scripts/CreditsButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        Debug.Log("Load Credits");
+        Application.LoadLevel(Application.levelCount - 1);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
