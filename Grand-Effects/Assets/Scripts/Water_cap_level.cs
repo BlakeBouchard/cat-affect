f4c26b4648938c05570fe5a76ced97bf0476ba3d
afroholic78 <mickael.zerihoun@gmail.com>
2014-01-25 18:14:52 -0700

    Added all the prefabs for water mechanics
    Place original water position and goes up to pipe

--- Grand-Effects/Assets/Scripts/Water_cap_level.cs
+++ Grand-Effects/Assets/Scripts/Water_cap_level.cs
@@ -3,16 +3,18 @@ using System.Collections;
 
 public class Water_cap_level : MonoBehaviour {
 
-    public float cap = transform.position.y;
+    public double cap; 
 	// Use this for initialization
 	void Start () {
-        
+        cap = transform.position.y - 2.5;
+        //Debug.Log(cap);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
+        //Debug.Log(cap);
 	}
+
 
 
 }
